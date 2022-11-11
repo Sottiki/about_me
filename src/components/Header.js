b18b870b2e0ecc887087012e2695a17c5a3cd4a2
Sottiki <81673075+Sottiki@users.noreply.github.com>
2022-11-11 23:22:34 +0900

    作業開始

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
+import Menues from "./Menu";
+
+const Header = () => {
+    return <Box>
+        <Heading> Header</Heading>
+
+        <Menues />
+    </Box >
+
+}
+
+export default Header;
